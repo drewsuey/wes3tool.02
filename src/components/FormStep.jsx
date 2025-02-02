23673e63ba943706b59a7efe5ccca8f250956197
drewsuey <and1alts@gmail.com>
2025-02-02 18:42:04 -0500

    fixed alignment issues

--- src/components/FormStep.jsx
+++ src/components/FormStep.jsx
@@ -17,46 +17,51 @@ function FormStep({ onUpdate }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Site Size (sq. ft):
+    <form className="wes3-form" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="siteSize">Site Size (sq. ft):</label>
         <input
           type="number"
           name="siteSize"
+          id="siteSize"
           required
           data-tooltip-id="tooltip-siteSize"
           data-tooltip-content="Enter the total area of the site in square feet."
         />
         <Tooltip id="tooltip-siteSize" />
-      </label>
+      </div>
 
-      <label>
-        Number of Floors:
+      <div className="form-group">
+        <label htmlFor="floors">Number of Floors:</label>
         <input
           type="number"
           name="floors"
+          id="floors"
           required
           data-tooltip-id="tooltip-floors"
           data-tooltip-content="Include all levels, including basements, that require coverage."
         />
         <Tooltip id="tooltip-floors" />
-      </label>
+      </div>
 
-      <label>
-        Number of Staircases:
+      <div className="form-group">
+        <label htmlFor="stairs">Number of Staircases:</label>
         <input
           type="number"
           name="stairs"
+          id="stairs"
           required
           data-tooltip-id="tooltip-stairs"
           data-tooltip-content="Enter the total number of staircases across all floors."
         />
         <Tooltip id="tooltip-stairs" />
-      </label>
+      </div>
 
-      <label>
-        Interface Integration:
-        <input type="checkbox" name="interfaceIntegration" />
+      <div className="form-group checkbox-group">
+        <label>
+          <input type="checkbox" name="interfaceIntegration" />
+          Interface Integration
+        </label>
         <span
           data-tooltip-id="tooltip-interface"
           data-tooltip-content="Check this box if you need the system to integrate with other fire panels or alarms."
@@ -64,11 +69,13 @@ function FormStep({ onUpdate }) {
           ?
         </span>
         <Tooltip id="tooltip-interface" />
-      </label>
+      </div>
 
-      <label>
-        REACT Integration:
-        <input type="checkbox" name="reactIntegration" />
+      <div className="form-group checkbox-group">
+        <label>
+          <input type="checkbox" name="reactIntegration" />
+          REACT Integration
+        </label>
         <span
           data-tooltip-id="tooltip-react"
           data-tooltip-content="Enable REACT integration for remote monitoring and notifications."
@@ -76,7 +83,7 @@ function FormStep({ onUpdate }) {
           ?
         </span>
         <Tooltip id="tooltip-react" />
-      </label>
+      </div>
 
       <button type="submit">Next</button>
     </form>
@@ -84,4 +91,5 @@ function FormStep({ onUpdate }) {
 }
 
 export default FormStep;
+
 
